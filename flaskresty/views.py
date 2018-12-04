ec296fd512db2aa00047f98ae5fbf47f8f7e659e
Richard Mable <richardbmable@gmail.com>
2018-12-03 22:17:44 -0700

    added a products get a put endpoint

--- flaskresty/views.py
+++ flaskresty/views.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flaskresty import api, app
+
+products = {}
 
 class HelloWorld(Resource):
     def get(self):
         return {'ola': 'amigos'}
 
+class Product(Resource):
+    def get(self, product_id):
+        return {product_id: products[product_id]}
+
+    def put(self, product_id):
+        products[product_id] = request.form['data']
+        return {product_id: products[product_id]}
+
+
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(Product, '/products/<int:product_id>')
